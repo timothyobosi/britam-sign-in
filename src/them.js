c9abfa736e326e685bfee2ee30e4561861acd174
timothyobosi <timothyobosi8@gmail.com>
2025-08-27 12:43:52 +0300

    customize Material-UI

--- src/them.js
+++ src/them.js
@@ -2,12 +2,30 @@ import {createTheme} from '@mui/material/styles'
 
 export const theme = createTheme({
     palette:{
-
+        primary:{main:'#0865ce'},
+        secondary:{main:'#4CAF50'},
+        error:{main:'#d32f2f'},
+        background:{default:'#1a1a1a'}
     },
     components:{
+        MuiButton:{
+            styleOverrides:{
+                root:{
+                    borderRadius: '',
+                    textTransform:'',
+                    fontSize:'',
+                    padding:''
+                },
+            },
+        },
 
     },
     MuiTextField:{
-        
+        styleOverrides:{
+            root:{
+
+            }
+        }
+
     }
 })
